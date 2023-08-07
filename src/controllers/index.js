e9c8c3752b405936c5497b152c65e9471d37f626
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-07 15:07:06 +0200

    major changes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,14 +1,23 @@
 const { PrismaClient } = require("@prisma/client");
+const bcrypt=require('bcrypt')
 
 const prisma = new PrismaClient();
 
 exports.createStudent = async (req, res) => {
     const {regNo,password}= req.body
+    const hashedPassword= await bcrypt.hash(password,12)
   try {
+    const existingStudent = await prisma.student.findUnique({
+        where: { regNo },
+    });
+
+    if (existingStudent) {
+        return res.status(400).json({ message: "Student with the same regNo already exists" });
+    }
     await prisma.student.create({
       data:{
         regNo,
-        password
+        password:hashedPassword
       }
     });
     res.status(200).json({message:"Student created successfully"});
